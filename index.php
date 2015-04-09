914f29291be639ef00dc6057122879c86954a0cd
Mitsunori Inaba <m-inaba@phoenixdesign.jp>
2015-04-09 15:54:37 +0900

    add phpcs

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $values = array(
     'comment' => '',
 );
 
-$app->get('/', function() use($app, $values) {
+$app->get('/', function () use ($app, $values) {
     echo 'Hello';
     echo PhoenixDesign\Lib\Util::isAscii('hoge');
 
@@ -31,21 +31,21 @@ $app->get('/', function() use($app, $values) {
     $app->getLog()->fatal('log'); // - level 0
 
 });
-$app->get('/info', function() {
+$app->get('/info', function () {
     phpinfo();
 });
-$app->notFound(function(){
+$app->notFound(function () {
     echo '404 not found';
 });
-$app->get('/myclass/', function() use($app){
+$app->get('/myclass/', function () use ($app) {
     //(new \Controller\Page($app))->index();
     $c = new \Controller\Page($app);
     $c->index();
 });
-$app->get('/longlong/:name/', function() use($app) {
+$app->get('/longlong/:name/', function () use ($app) {
     myFunction($app);
 });
-$app->get('/json/', function() use($app) {
+$app->get('/json/', function () use ($app) {
     $result = array(
         'id' => '100',
         'name' => '名前',
@@ -54,25 +54,28 @@ $app->get('/json/', function() use($app) {
     $app->response->headers->set('Content-Type', 'application/json');
     echo json_encode($result);
 });
-$app->get('/hello/:name/', function($name){
+$app->get('/hello/:name/', function ($name) {
     echo 'Hello ' . $name;
 });
-$app->get('/redirect/', function() use($app){
+$app->get('/redirect/', function () use ($app) {
     $app->redirect(\Config::$app_path . 'test/');
 });
-$app->get('/test/', function() use ($app, $values) {
+$app->get('/test/', function () use ($app, $values) {
     $values['page_title'] = 'ページ';
     $values['id'] = '101';
     $values['name'] = '名前';
 
     $app->render('test.php', array('values' => $values));
 });
-$app->post('/post_test/', function() use($app, $values) {
+$app->post('/post_test/', function () use ($app, $values) {
     $values['name'] = $app->request()->post('name');
     $values['age'] = $app->request()->post('age');
     $sex = '';
-    if (NULL == $app->request()->post('sex')) echo '性別が選択されていません';
-    else $values['sex'] = $app->request()->post('sex');
+    if (null == $app->request()->post('sex')) {
+        echo '性別が選択されていません';
+    } else ${
+        values['sex'] = $app->request()->post('sex');
+    }
     echo 'test name->' . $values['name'] . ',age->' . $values['age'] . ',性別->' . $values['sex'];
 });
 
